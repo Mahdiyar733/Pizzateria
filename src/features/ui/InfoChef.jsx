3a0917aa17e5b95da41eff606da56f4d55822625
Mahdiyar733 <makhti733@gmail.com>
2024-09-05 21:28:50 +0330

    chef info page

--- src/features/ui/InfoChef.jsx
+++ src/features/ui/InfoChef.jsx
@@ -14,33 +14,42 @@ function InfoChef() {
 	return (
 		<div className="h-auto w-full bg-white bg-opacity-50 px-6 py-20 flex items-center flex-col">
 			<div className="flex flex-col md:flex-row w-auto items-start px-5 gap-1">
-				<img
-					className="max-w-60 rounded-lg mb-4 self-center"
-					src={chef.src}
-					alt={chef.name}
-				/>
-				<h2 className="text-xl text-black font-semibold ">{chef.name}</h2>
-				<h3 className="text-RED font-semibold text-sm mb-4">{chef.job}</h3>
-				<h4 className="text-black font-normal text-base">
-					<span className="font-semibold text-black">Work experience : </span>{" "}
-					<span className="text-xl text-RED font-semibold">
-						{chef.workYear}
-					</span>{" "}
-					years
-				</h4>
-				<h4 className="text-black font-normal text-base">
-					<span className="font-semibold text-black mb-1">Age : </span>{" "}
-					<span className="text-xl text-RED font-semibold">{chef.age}</span>{" "}
-					years old
-				</h4>
-				<p className="text-black text-base text-left font-semibold text-opacity-60 tracking-wide pt-4 mt-4 border-t border-solid border-RED">
-					{chef.des}
-				</p>
-				<button
-					onClick={() => nav(-1)}
-					className="px-6 mt-5 py-2 bg-RED text-white rounded-lg">
-					Back
-				</button>
+				<div className="w-full flex flex-col items-center md:flex-1">
+					<img
+						className="max-w-60 rounded-lg mb-4 self-center"
+						src={chef.src}
+						alt={chef.name}
+					/>
+					<h2 className="text-xl text-black font-semibold ">{chef.name}</h2>
+					<h3 className="text-RED font-semibold text-sm mb-4 ">{chef.job}</h3>
+					<div className="flex flex-col bg-RED p-3 gap-1 rounded-lg w-full max-w-[393px] md:max-w-[260px]">
+						<h4 className="text-white font-normal text-base flex justify-between">
+							<span className="font-semibold text-white">
+								Work experience :{" "}
+							</span>{" "}
+							<span className="text-base text-[#2B2B2B] tracking-wide font-semibold">
+								{chef.workYear} years
+							</span>
+						</h4>
+						<h4 className="text-white font-normal text-base flex justify-between">
+							<span className="font-semibold text-white">Age : </span>{" "}
+							<span className="text-base text-[#2B2B2B] tracking-wide font-semibold">
+								{chef.age} years old
+							</span>
+						</h4>
+					</div>
+				</div>
+				<div className=" relative flex flex-col mt-4 border-t border-solid border-RED md:mt-0 md:border-t-0 md:border-l md:flex-1 lg:flex-[2] md:max-h-[396px] md:min-h-[396px] overflow-y-scroll">
+					<p className="text-black text-base text-left font-semibold text-opacity-60 tracking-wide pt-4 md:pt-0 md:px-7 lg:text-lg lg:font-normal">
+						{chef.des}
+					</p>
+					<button
+						onClick={() => nav(-1)}
+						style={{ zIndex: 100 }}
+						className="px-6 mt-5 py-2 bg-RED text-white rounded-lg w-32 md:ml-4 fixed bottom-3 left-3">
+						Back
+					</button>
+				</div>
 			</div>
 		</div>
 	);
