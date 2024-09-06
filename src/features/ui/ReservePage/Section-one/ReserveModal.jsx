dffb8943d8c6810a6fd34f6eddabe8f6e975efe8
Mahdiyar733 <makhti733@gmail.com>
2024-09-06 15:27:09 +0330

    reserve form

--- src/features/ui/ReservePage/Section-one/ReserveModal.jsx
+++ src/features/ui/ReservePage/Section-one/ReserveModal.jsx
@@ -5,19 +5,23 @@ function ReserveModal({ isVisible, children }) {
 	return (
 		<AnimatePresence>
 			{isVisible && (
-				<div
-					className="w-dvw h-dvh backdrop-blur-[2px] backdrop-brightness-50 justify-center items-center flex fixed top-0 right-0 left-0 bottom-0"
-					style={{ zIndex: 1000 }}>
+				<motion.div
+					className="w-dvw h-dvh backdrop-blur-[1px] backdrop-brightness-50 justify-center items-center flex fixed top-0 right-0 left-0 bottom-0"
+					style={{ zIndex: 1000 }}
+					initial={{ opacity: 0, scale: 0.95 }}
+					animate={{ opacity: 1, scale: 1 }}
+					exit={{ opacity: 0, scale: 0.95 }}
+					transition={{ type: "just", delayChildren: 0.3 }}>
 					<motion.div
 						initial={{ opacity: 0, scale: 0.2 }}
 						animate={{ opacity: 1, scale: 1 }}
-						exit={{ opacity: 0, scale: 0.2 }}
-						transition={{ type: "spring", stiffness: 85 }}
+						exit={{ opacity: 0, scale: 0.8 }}
+						transition={{ type: "just" }}
 						style={{ zIndex: 100 }}
 						className="w-80 h-80 bg-gray-200 rounded-lg p-7 flex flex-col items-center gap-3">
 						{children}
 					</motion.div>
-				</div>
+				</motion.div>
 			)}
 		</AnimatePresence>
 	);
--- src/features/ui/ReservePage/Section-one/Section1.jsx
+++ src/features/ui/ReservePage/Section-one/Section1.jsx
@@ -1,149 +1,191 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import TablePic from "./table.png";
 import LittleTitle from "../../../utils/LittleTitle";
 import BigTitle from "../../../utils/BigTitle";
 import MotionDiv from "../../../utils/MotionDiv";
 import ReserveModal from "./ReserveModal";
+import { ScrollUp } from "../../../utils/helpers";
+import NameSvg from "../../../../svg/NameSvg";
+import PhoneSvg from "../../../../svg/PhoneSvg";
+import FamilySvg from "../../../../svg/FamilySvg";
+
+const ReserveContext = createContext();
 
 function Section1() {
 	const [ordererName, setOrederName] = useState("");
 	const [phone, setPhone] = useState("");
 	const [NumberPpl, setNumberPpl] = useState("");
-	const [isOpenAlert, setIsOpenAlert] = useState(false);
 	const [isOpenModalBox, setIsOpenModalBox] = useState(false);
 	const [btnIsDisable, setBtnIsDisable] = useState(false);
+	const bodyRef = useRef();
 
 	function handleSubmit(e) {
 		if (!ordererName.trim() || !phone.trim() || !NumberPpl.trim()) return;
+		if (ordererName.length >= 10 || phone.length >= 12 || NumberPpl.length >= 3)
+			return;
 		e.preventDefault();
+		ScrollUp();
 		setIsOpenModalBox(true);
 		setBtnIsDisable(true);
 	}
 
 	useEffect(() => {
-		const timer = setTimeout(() => {
-			setIsOpenAlert(false);
-		}, 3500);
+		bodyRef.current = document.body;
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [isOpenAlert]);
+		if (isOpenModalBox) {
+			bodyRef.current.style = "overflow: hidden";
+		} else {
+			bodyRef.current.style = "overflow: auto";
+		}
 
-	useEffect(() => {
-		const timerw = setTimeout(() => {
-			setBtnIsDisable(false);
-		}, 4400);
+		if (!isOpenModalBox) {
+			const timerw = setTimeout(() => {
+				setBtnIsDisable(false);
+			}, 1);
 
-		return () => {
-			clearTimeout(timerw);
-		};
-	}, [btnIsDisable]);
+			return () => {
+				clearTimeout(timerw);
+			};
+		}
+	}, [isOpenModalBox]);
 
 	return (
-		<div className="relative h-auto bg-RED w-full flex flex-col py-9 px-10 md:px-14 base:px-20 sm:py-8 md:py-16 gap-5 justify-evenly lg:py-12 lg:px-32 xl:px-56 base:gap-3 xl:gap-5 md:gap-2">
-			<div className="flex flex-col items-center gap-3 md:items-start animate-fade-up animate-duration-700">
-				<LittleTitle color="text-white">reservation</LittleTitle>
-				<BigTitle
-					pos="text-center md:text-start"
-					font="font-bold">
-					Turn your dining dreams into <br /> reality by booking a table with us
-				</BigTitle>
-			</div>
-			<div className="flex flex-row items-center justify-center md:justify-between gap-8">
-				<MotionDiv type="1">
-					<form className="flex flex-col items-center gap-3 sm:gap-5 md:items-start">
-						<h2 className="text-white text-xl font-bold md:text-2xl">
-							Reservation <span className="text-black">Now!</span>
-						</h2>
-						<input
-							value={ordererName}
-							onChange={(e) => setOrederName(e.target.value)}
-							type="text"
-							className="w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none transition-all duration-300 px-3 capitalize"
-							placeholder="Orderer's name"
-						/>
-						<div className="flex flex-row gap-3 sm:gap-5 md:gap-3">
+		<ReserveContext.Provider
+			value={{
+				ordererName,
+				phone,
+				NumberPpl,
+				isOpenModalBox,
+				setIsOpenModalBox,
+				setOrederName,
+				setPhone,
+				setNumberPpl,
+			}}>
+			<div className="relative h-auto bg-RED w-full flex flex-col py-9 px-10 md:px-14 base:px-20 sm:py-8 md:py-16 gap-5 justify-evenly lg:py-12 lg:px-32 xl:px-56 base:gap-3 xl:gap-5 md:gap-2">
+				<div className="flex flex-col items-center gap-3 md:items-start animate-fade-up animate-duration-700">
+					<LittleTitle color="text-white">reservation</LittleTitle>
+					<BigTitle
+						pos="text-center md:text-start"
+						font="font-bold">
+						Turn your dining dreams into <br /> reality by booking a table with
+						us
+					</BigTitle>
+				</div>
+				<div className="flex flex-row items-center justify-center md:justify-between gap-8">
+					<MotionDiv type="1">
+						<form className="flex flex-col items-center gap-3 sm:gap-5 md:items-start">
+							<h2 className="text-white text-xl font-bold md:text-2xl">
+								Reservation <span className="text-black">Now!</span>
+							</h2>
 							<input
-								value={phone}
-								onChange={(e) => setPhone(e.target.value)}
-								type="tel"
-								placeholder="Phone number"
-								className="flex-1 w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none focus:flex-[2] transition-all duration-300 px-3"
+								value={ordererName}
+								onChange={(e) => setOrederName(e.target.value)}
+								type="text"
+								className="w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none transition-all duration-300 px-3 capitalize"
+								placeholder="Orderer's name"
 							/>
-							<input
-								value={NumberPpl}
-								onChange={(e) => {
-									if (e.target.value > 0) {
-										setNumberPpl(e.target.value);
-									} else if (e.target.value == "") {
-										setNumberPpl("");
-									} else {
-										return;
-									}
-								}}
-								placeholder="Number of people"
-								type="number"
-								className="flex-1 w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none focus:flex-[2] transition-all duration-300 px-3"
-							/>
-						</div>
-						{!btnIsDisable ? (
-							<button
-								className="bg-black text-white rounded-full px-5 py-2.5 lg:py-3 lg:px-7 text-xs hover:bg-PINK hover:text-black transition-all duration-300 animate-fade"
-								onClick={(e) => handleSubmit(e)}
-								type="button">
-								Reservation
-							</button>
-						) : (
-							<button
-								disabled="true"
-								className="bg-black text-white rounded-full px-5 py-2.5 lg:py-3 lg:px-7 text-xs"
-								type="button">
-								Reservation
-							</button>
-						)}
-					</form>
-				</MotionDiv>
-				<img
-					className="hidden md:block object-cover h-80 base:-mt-20 animate-fade-left animate-duration-700"
-					src={TablePic}
-				/>
+							<div className="flex flex-row gap-3 sm:gap-5 md:gap-3">
+								<input
+									value={phone}
+									onChange={(e) => setPhone(e.target.value)}
+									type="tel"
+									placeholder="Phone number"
+									className="flex-1 w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none focus:flex-[2] transition-all duration-300 px-3"
+								/>
+								<input
+									value={NumberPpl}
+									onChange={(e) => {
+										if (e.target.value > 0) {
+											setNumberPpl(e.target.value);
+										} else if (e.target.value == "") {
+											setNumberPpl("");
+										} else {
+											return;
+										}
+									}}
+									placeholder="Number of people"
+									type="number"
+									className="flex-1 w-full bg-white md:text-sm py-2 placeholder:font-normal rounded-md text-black font-normal text-xs focus:outline-none focus:flex-[2] transition-all duration-300 px-3"
+								/>
+							</div>
+							{!btnIsDisable ? (
+								<button
+									className="bg-black text-white rounded-full px-5 py-2.5 lg:py-3 lg:px-7 text-xs hover:bg-PINK hover:text-black transition-all duration-300 animate-fade"
+									onClick={(e) => handleSubmit(e)}
+									type="button">
+									Reservation
+								</button>
+							) : (
+								<button
+									disabled={true}
+									className="bg-black text-white rounded-full px-5 py-2.5 lg:py-3 lg:px-7 text-xs"
+									type="button">
+									Reservation
+								</button>
+							)}
+						</form>
+					</MotionDiv>
+					<img
+						className="hidden md:block object-cover h-80 base:-mt-20 animate-fade-left animate-duration-700"
+						src={TablePic}
+					/>
+				</div>
+				<ModalBox />
 			</div>
-			<ModalBox
-				ordererName={ordererName}
-				phone={phone}
-				NumberPpl={NumberPpl}
-				setIsOpenModalBox={setIsOpenModalBox}
-				isOpenModalBox={isOpenModalBox}
-			/>
-		</div>
+		</ReserveContext.Provider>
 	);
 }
 
-function ModalBox({
-	ordererName,
-	phone,
-	NumberPpl,
-	setIsOpenModalBox,
-	isOpenModalBox,
-}) {
+function ModalBox() {
+	const {
+		ordererName,
+		phone,
+		NumberPpl,
+		setIsOpenModalBox,
+		isOpenModalBox,
+		setOrederName,
+		setPhone,
+		setNumberPpl,
+	} = useContext(ReserveContext);
+
+	function handleSubmit() {
+		setIsOpenModalBox(false);
+		setOrederName("");
+		setNumberPpl("");
+		setPhone("");
+	}
+
+	function handleClose(e) {
+		e.preventDefault();
+		setIsOpenModalBox(false);
+	}
+
 	return (
 		<ReserveModal isVisible={isOpenModalBox}>
 			<span className="text-white text-xs font-normal w-full bg-RED p-3 rounded-lg flex flex-row items-center justify-between">
-				Orderer&apos;s name :{" "}
-				<span className="text-xs bg-white text-RED py-1 px-4 rounded-md">
+				<div className="flex flex-row items-center justify-center gap-2">
+					<NameSvg />
+					<span>Orderer&apos;s name :</span>
+				</div>
+				<span className="text-xs bg-white text-RED py-1 px-4 rounded-md capitalize">
 					{ordererName}
 				</span>
 			</span>
 			<span className="text-white text-xs font-normal w-full bg-RED p-3 rounded-lg flex flex-row items-center justify-between">
-				Phone :{" "}
+				<div className="flex flex-row items-center justify-center gap-2">
+					<PhoneSvg />
+					<span>Phone :</span>
+				</div>
 				<span className="text-xs bg-white text-RED py-1 px-4 rounded-md">
 					{phone}
 				</span>
 			</span>
 			<span className="text-white text-xs font-normal w-full bg-RED p-3 rounded-lg flex flex-row items-center justify-between">
-				Number of people{" "}
+				<div className="flex flex-row items-center justify-center gap-2">
+					<FamilySvg />
+					<span>Number of people :</span>
+				</div>
 				<span className="text-xs bg-white text-RED py-1 px-4 rounded-md">
 					{NumberPpl}
 				</span>
@@ -155,12 +197,20 @@ function ModalBox({
 				</span>
 			</span>
 			<div className="w-full flex flex-row gap-3 items-center justify-center mt-2">
-				<button className="px-12 text-sm py-2 border border-RED border-solid bg-RED text-white font-normal rounded-md">
-					Pay
-				</button>
+				<a
+					href="https://investor.pypl.com/home/"
+					target="_blank">
+					<button
+						onClick={handleSubmit}
+						className="px-12 text-sm py-2 border border-RED border-solid bg-RED text-white font-normal rounded-md hover:bg-PINK hover:border-white transition-all duration-300 hover:px-16"
+						type="button">
+						Pay
+					</button>
+				</a>
 				<button
-					onClick={() => setIsOpenModalBox(false)}
-					className="px-6 text-sm py-2 bg-transparent border border-RED border-solid text-RED font-normal rounded-md">
+					onClick={handleClose}
+					className="px-6 text-sm py-2 bg-transparent border border-RED border-solid text-RED font-normal rounded-md hover:bg-RED transition-all duration-300 hover:border-white hover:px-10 hover:text-white"
+					type="button">
 					Cancel
 				</button>
 			</div>
